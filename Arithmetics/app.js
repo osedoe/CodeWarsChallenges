bf5ce041373f0cb0c6b25fe549a65f9c4b622c6e
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-03-09 20:04:53 +0000

    [Arithmetics] Added and solved

---
+++ Arithmetics/app.js
@@ -0,0 +1,26 @@
+const arithmetic = (a, b, operator) => {
+    let answer;
+    switch (operator) {
+        case "add":
+            answer = a+b;
+            break;
+
+        case "subtract":
+            answer = a-b;
+            break;
+
+        case "multiply":
+            answer = a*b;
+            break;
+
+        case "divide":
+            answer = a/b;
+            break;
+    }
+    return answer;
+};
+
+arithmetic(8,2,"subtract");
+
+
+

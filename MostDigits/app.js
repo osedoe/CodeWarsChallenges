f97e9924036aaccfb99fd8519b80b8be086f4053
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-03-10 23:38:37 +0000

    [MostDigits] Solved, since last solution didn't pass all the tests

--- MostDigits/app.js
+++ MostDigits/app.js
@@ -1,7 +1,23 @@
-const findLongest = (array) => {
-    return Math.max(...array);
+
+// const findLongest = a => Math.max(...a);
+//
+// The above solution won't pass the second condition of the question
+// since it finds the biggest number (value) in an array,
+// not the first one with the most digits.
+
+const findLongest = array => {
+    let digitLength = 0;
+    let firstLongest;
+    for (let i = 0; i < array.length; i++) {
+        let str = array[i].toString();
+        if (str.length > digitLength) {
+            digitLength = str.length;
+            firstLongest = str;
+        }
+    }
+    console.log(firstLongest);
+    return firstLongest;
 };
-  
 
 findLongest([1, 10, 100]); // 100
 findLongest([9000, 8, 800]); // 9000

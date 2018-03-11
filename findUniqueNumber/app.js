44ac2614e7c6ba0190ee932d025560d93d0c1053
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-03-11 22:26:35 +0000

    [FindUniqueNumber] Solved

--- findUniqueNumber/app.js
+++ findUniqueNumber/app.js
@@ -1,15 +1,13 @@
-let findUniq = (arr) => {
-    
-    for(let i=0; i<arr.length; i++) {
-        for (let j=0; j<arr.length; j++) {
-            if (arr[i] === arr[j]) {
-                arr.splice(i, 1);
-                arr.splice(j, 1);
-            }
-        }    
-    }  
-    console.log(arr[0]);
-    return arr[0];
+let findUniq = arr => {
+    let lng = arr.length;
+    let sortedArr = arr.sort( (a,b) => a - b);
+    if (sortedArr[0] === sortedArr[lng-2]) {
+        sortedArr.splice(0, lng-1);
+        return sortedArr[0];
+    } else if (sortedArr[1] === sortedArr[lng-2]) {
+        sortedArr.splice(1, lng-1);
+        return sortedArr[0];
+    }
 };
 
 findUniq([ 0, 1, 0 ]);

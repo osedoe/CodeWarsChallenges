75c496f64e0dea65365e26dd272933957fc81f82
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-07-25 13:02:27 +0100

    [uniqueInOrder] Refactor solution

--- src/UniqueInOrder/unique-in-order.js
+++ src/UniqueInOrder/unique-in-order.js
@@ -1,22 +1,16 @@
 function uniqueInOrder(iterable) {
-    // Check that iterable is an array
-    let arr;
+    // Check that iterable is an array or a string and turn it into an array if string
+    let arr = iterable;
     if (typeof iterable === 'string') {
-        arr = iterable.split(''); // ​​​​​[ 'A', 'B', 'B', 'C', 'c', 'A', 'D' ]​​​​​
-    } else {
-        arr = iterable;
+        arr = iterable.split('');
+    // } else {
+    //     arr = iterable;
     }
     let result = [];
-    
     for (let value of arr) {
-
-        if (result.indexOf(value) === -1 || result.lastIndexOf(value) !== result.length-1) {
-            result.push(value);
-        }
+        if (result.indexOf(value) === -1 || result.lastIndexOf(value) !== result.length-1) result.push(value);
     }
-    console.log(result);
     return result;
 }
 
 module.exports = uniqueInOrder;
-// uniqueInOrder('ABBCcAD');

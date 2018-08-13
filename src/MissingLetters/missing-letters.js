5f6cb84bbd47569bcc3b1575732f9a0da2abd949
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-08-13 15:13:27 +0100

    [MissingLetters] Refactorize solution

--- src/MissingLetters/missing-letters.js
+++ src/MissingLetters/missing-letters.js
@@ -1,27 +1,18 @@
 function fearNotLetter(str) {
 	
 	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-	const alphabetArray = alphabet.split('');
-	// console.log(alphabetArray);
-	const strArray = str.split('');
-	// console.log(strArray);
-	
-	const startIndex = alphabetArray.indexOf(strArray[0]);
-	// console.log(startIndex);
-	const lastLetter = strArray[(strArray.length-1)];
-	// console.log(lastLetter);
-	const lastIndex = alphabet.indexOf(lastLetter);
-	// console.log(lastIndex);
 
+	const alphabetArray = alphabet.split('');
+	const strArray = str.split('');
+
+	const startIndex = alphabetArray.indexOf(strArray[0]);
+	const lastLetter = strArray[(strArray.length-1)];
+	const lastIndex = alphabet.indexOf(lastLetter);
 	const subAlphabetArray = alphabetArray.slice(startIndex, lastIndex+1);
-	// console.log(subAlphabetArray);
-	
-	const result = subAlphabetArray.filter(letter => {
-		return strArray.indexOf(letter) === -1;
-	});
+
+	const result = subAlphabetArray.filter(letter => strArray.indexOf(letter) === -1);
+
 	return result[0];
 }
 
 module.exports = fearNotLetter;
-
-// fearNotLetter("abcefg");

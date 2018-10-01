ba3717677bd1d779a941bd39a614beb6c94ca1ca
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-10-02 00:24:30 +0100

    Quick fix in DropIt

---
+++ src/_DropIt/drop-it.js
@@ -0,0 +1,11 @@
+function dropElements(arr, func) {
+
+	
+
+	
+}
+  
+module.exports = dropElements;
+
+dropElements([1, 2, 3], function(n) {return n < 3; }); // [1, 2, 3]
+dropElements([1, 2, 3], function (n) {return n >= 3; }); // [3]

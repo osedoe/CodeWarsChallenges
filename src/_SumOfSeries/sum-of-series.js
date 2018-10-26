dc35b5716c591c00ba8021876d46ada7217a1682
Samanta <slaragiannone@gmail.com>
2018-10-26 20:14:03 +0100

    sumseries done

--- src/_SumOfSeries/sum-of-series.js
+++ src/_SumOfSeries/sum-of-series.js
@@ -1,4 +1,23 @@
-const seriesSum = n => {
-	// Some code
-	return result;
-};
+const seriesSum = (value) => {
+    let sum = 1;
+  
+    if ( !typeof(value) == Number || value <= 0 || !Number.isInteger(value) ) {
+      console.log('unvalid entry');
+    }
+  
+    else if ( value == 1) {
+      return value.toFixed(2).toString();
+    }
+  
+    else {
+      for ( let i = 1; i <= value; i++) {
+        let k = i + 3;
+        sum = sum + 1/k;
+      }
+
+      let result = sum.toFixed(2);
+      return result.toString();
+    }   
+  };
+
+module.exports = sumPrimes;s

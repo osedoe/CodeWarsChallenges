dd8ec3b9306eb646c6b4ca3cf1926a435e491c5c
Samanta <slaragiannone@gmail.com>
2018-11-07 13:05:18 +0000

    _smallestCommunMultiple doesn't work yet

--- src/_SmallestCommonMultiple/readme.md
+++ src/_SmallestCommonMultiple/readme.md
@@ -6,5 +6,8 @@ The range will be an array of two numbers that will not necessarily be in numeri
 
 For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
 
-I used that ![scm](http://latex.codecogs.com/png.latex?scm%28a%2Cb%29%3D%5Cfrac%7B%7Ca%20%5Ccdot%20b%7C%7D%7Bgcd%28a%2Cb%29%7D)
+I used that 
+
+![scm](http://latex.codecogs.com/png.latex?scm%28a%2Cb%29%3D%5Cfrac%7B%7Ca%20%5Ccdot%20b%7C%7D%7Bgcd%28a%2Cb%29%7D)
+
 where gcd(a,b) is the greatest common divisor (the largest positive integer that divides each of a and b).
--- src/_SmallestCommonMultiple/scm.js
+++ src/_SmallestCommonMultiple/scm.js
@@ -1,22 +1,53 @@
-function dividers (value) {
+function obtainDividers(value) {
 	let div = [];
+
 	for (let i = 1; i < value; i++) {
 		if (value % i == 0) {
-			push.div(i);
+			div.push(i);
 		}
 	}
 
+	return div;
+}
+
+function minimumDividers (value) {
+	let mdiv = obtainDividers(value); 
+	for (let i = 0; i < mdiv.length-1; i++) {
+		for (let j = 1; j < mdiv.length; j++) {
+			if (mdiv[i] % mdiv[j] == 0 && mdiv[i] !== mdiv[j]) {
+				mdiv.pop(i);
+			}
+		}
+	}
+
+	return mdiv;
+}
+
+function compare(array1,array2) {
+	var common = [];
+	for (let i = 0; i < array1.length; i++) {
+	  for (let j = 0; j < array2.length; j++) {
+	  	if ( array1[i] == array2[j] | array2[j]%array1[i] == 0) {
+		  common.push(array1[i]);
+		  }
+	  }
+  } 
+  
+	return common;
 }
 
 function greatestCommonDivisor (arr) {
-	let a = arr[0];
-	let b = arr[1];
-	
-	return  gcm;
+	let mdiv1 = minimumDividers(arr[0]);
+  	let mdiv2 = minimumDividers(arr[1]);
+	let gcd = compare(mdiv1,mdiv2);
+
+	return  gcd;
 }
 
-function smallestCommonMultiple(arr,gcm) {
-	return scm;
+function smallestCommonMultiple (arr) {
+	let gcd = greatestCommonDivisor (arr);
+
+    return arr[0]*arr[1]/gcd;
 }
 	
 module.exports = smallestCommmonMultiple;

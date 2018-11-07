191e00f2e54dbba3ab8996a86abd86fa4ac1f09c
Samanta <slaragiannone@gmail.com>
2018-11-07 12:13:20 +0000

    _smallestCommunMultiple modified

--- src/_SmallestCommonMultiple/scm.js
+++ src/_SmallestCommonMultiple/scm.js
@@ -1,7 +1,22 @@
-function smallestCommons(arr) {
-	return arr;
+function dividers (value) {
+	let div = [];
+	for (let i = 1; i < value; i++) {
+		if (value % i == 0) {
+			push.div(i);
+		}
+	}
+
+}
+
+function greatestCommonDivisor (arr) {
+	let a = arr[0];
+	let b = arr[1];
+	
+	return  gcm;
+}
+
+function smallestCommonMultiple(arr,gcm) {
+	return scm;
 }
 	
-module.exports = smallestCommons;
-
-smallestCommons([1,5]);
+module.exports = smallestCommmonMultiple;

1703958a7b185596a3de1a7b593e57c6bb2d26b3
Samanta <slaragiannone@gmail.com>
2018-11-07 22:52:47 +0000

    change of name -> resolved katas

---
+++ src/SumAllPrimes/sum-all-primes.js
@@ -0,0 +1,22 @@
+function sumPrimes(value) {
+  var primes = [];
+  let sum = 0;
+
+  for (let i = 2; i <= value; i++) {
+    let a = 0;
+    for (let j = 2; j < i; j++) {
+      if (i%j == 0) {
+        a++;
+      }
+    }
+    if ( a == 0 ) {
+      primes.push(i);
+    }
+  }
+  for (let i = 0; i < primes.length; i++) {
+    sum = sum + primes[i]; 
+  }
+return sum;
+}
+
+module.exports = sumPrimes;

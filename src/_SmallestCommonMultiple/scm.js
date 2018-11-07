0d31f44696016b3b003bdc5629767433717f7432
Samanta <slaragiannone@gmail.com>
2018-11-07 14:45:17 +0000

    _smallestCommunMultiple working

--- src/_SmallestCommonMultiple/scm.js
+++ src/_SmallestCommonMultiple/scm.js
@@ -1,7 +1,7 @@
 function obtainDividers(value) {
 	let div = [];
 
-	for (let i = 1; i < value; i++) {
+	for (let i = 1; i <= value; i++) {
 		if (value % i == 0) {
 			div.push(i);
 		}
@@ -14,7 +14,7 @@ function minimumDividers (value) {
 	let mdiv = obtainDividers(value); 
 	for (let i = 0; i < mdiv.length-1; i++) {
 		for (let j = 1; j < mdiv.length; j++) {
-			if (mdiv[i] % mdiv[j] == 0 && mdiv[i] !== mdiv[j]) {
+			if (mdiv[i] % mdiv[j] == 0 && mdiv[i] == mdiv[j]) {
 				mdiv.pop(i);
 			}
 		}
@@ -27,13 +27,18 @@ function compare(array1,array2) {
 	var common = [];
 	for (let i = 0; i < array1.length; i++) {
 	  for (let j = 0; j < array2.length; j++) {
-	  	if ( array1[i] == array2[j] | array2[j]%array1[i] == 0) {
+	  	if ( array1[i] == array2[j] ) {
 		  common.push(array1[i]);
-		  }
+		}
 	  }
-  } 
+    } 
   
-	return common;
+	let gcd = 1;
+	for (let i=0; i < common.length; i++) {
+		gcd *= common[i];
+	}
+
+	return gcd;
 }
 
 function greatestCommonDivisor (arr) {
@@ -45,6 +50,12 @@ function greatestCommonDivisor (arr) {
 }
 
 function smallestCommonMultiple (arr) {
+	let aux;
+	if (arr[1] > arr[0]) {
+		aux = arr[0];
+		arr[0] = arr[1];
+		arr[1] = aux;
+	}
 	let gcd = greatestCommonDivisor (arr);
 
     return arr[0]*arr[1]/gcd;

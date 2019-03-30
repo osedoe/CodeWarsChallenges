aee0ae788211a90ead1341e733051dbe94c955ca
Ose Diaz Gonzalez <jose.diazg@protonmail.com>
2019-03-30 21:35:38 +0000

    Add smallEnough

---
+++ src/_SmallEnough/small-enough.ts
@@ -0,0 +1,3 @@
+function smallEnough(a, limit){
+
+}
---
+++ tests/small-enough.test.ts
@@ -0,0 +1,6 @@
+// Test.describe("Example tests",_=>{
+//     Test.assertEquals(smallEnough([66, 101], 200), true);
+//     Test.assertEquals(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100), false);
+//     Test.assertEquals(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
+//     Test.assertEquals(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120), true);
+// })

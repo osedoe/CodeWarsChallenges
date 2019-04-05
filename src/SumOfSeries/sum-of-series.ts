ddc69aff5aabcd2fcc2aa5e0b28d5cc25e9aa67d
Ose Diaz Gonzalez <jose.diazg@protonmail.com>
2019-04-05 21:05:23 +0100

    Fix tiny error in kata

--- src/SumOfSeries/sum-of-series.ts
+++ src/SumOfSeries/sum-of-series.ts
@@ -1,15 +1,11 @@
-// FIXME: Check error
-export function seriesSum(value) {
+export function seriesSum(value: number) {
   let sum = 1;
-  if (!typeof (value) == Number || value <= 0 || !Number.isInteger(value)) {
-    console.log('unvalid entry');
-  } else if (value == 1) {
-    return value.toFixed(2);
-  } else {
-    for (let i = 1; i < value; i++) {
-      let k = 1+3*i;
-      sum = sum + 1/k;
-    }
-    return sum.toFixed(2);
+  if (value === 1) {
+    return value.toFixed(1);
   }
+  for (let i = 1; i < value; i++) {
+    let k = 1 + 3 * i;
+    sum = sum + 1 / k;
+  }
+  return sum.toFixed(2);
 }

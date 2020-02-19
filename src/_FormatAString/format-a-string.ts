30fd36c837b1e1c78d454195fb61a0b9df6d33c8
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2020-02-19 23:46:28 +0000

    Add solution for format-a-string

--- src/_FormatAString/format-a-string.ts
+++ src/_FormatAString/format-a-string.ts
@@ -1,3 +1,18 @@
-export function formatAString(names){
-    //your code here
-  }
+interface NameObject {
+  name: string;
+}
+
+export const formatAString = (namesList: NameObject[]): string => {
+  const regExp = {
+    lastComma: /,(?=[^,]*$)/,
+    allCommas: /,+/g,
+    ampersand: /&/
+  };
+
+  return namesList
+    .map(object => object.name)
+    .join(',')
+    .replace(regExp.lastComma, '&')
+    .replace(regExp.allCommas, ', ')
+    .replace(regExp.ampersand, ' & ');
+};

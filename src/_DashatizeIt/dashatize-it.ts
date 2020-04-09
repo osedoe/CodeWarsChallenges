7a8dbb14d72971eabb889f5771ffb87cbd0fafb2
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2020-04-09 18:40:19 +0100

    CLeanup

--- src/_DashatizeIt/dashatize-it.ts
+++ src/_DashatizeIt/dashatize-it.ts
@@ -1,3 +1,18 @@
-export function dashatize(num) {
-    // code
+export const dashatize = (number: number): string => {
+  const result = number
+    .toString()
+    .split('')
+    .map(elem => +elem % 2 === 0 ? `${ elem }-` : elem)
+    .map((elem, index, array) => {
+      if (array.indexOf(elem) === 0 || array.lastIndexOf(elem) === array.length-1) {
+        return false;
+      }
+      return elem;
+    })
+    .join('');
+    // .replace(/^-/);
+  console.log('🍓', result);
+
+  return '';
+
 };
